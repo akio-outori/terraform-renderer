f335d2ffb0af8327c6889ff2da0b0620c52b2bc6
Jeff Hallyburton <jeff.hallyburton@thomsonreuters.com>
2017-02-19 13:26:56 -0500

    Simplify how template variables are handled and introduce error handling for missing template entries.

--- includes/terraform_generator.py
+++ includes/terraform_generator.py
@@ -13,47 +13,36 @@ class terraform_generator():
   def __init__(self, template, system):
     self.data                        = self.__read_yaml(template)
     self.parent_dir                  = self.__createDir(template, system)
-    self.access_key, self.secret_key = self.__getAWSCredentials()
-    self.__constructVars()
-    self.__constructInstance()
-    self.__constructSG()
-    print(self.access_key + " " + self.secret_key)
 
   def __read_yaml(self, template):
     with open(template) as f:
       data = yaml.safe_load(f)  
     return data
 
-  def __constructVars(self):
-    self.region  = self.data['variables']['region']
-    self.vpc     = self.data['variables']['vpc']
-    self.subnet  = random.choice(self.data['variables']['subnet'].values())
-    self.ami     = self.data['variables']['ami']
-    self.keypair = self.data['variables']['keypair']
-    self.user    = self.data['variables']['user']
-
-  def __constructInstance(self):
-    self.instance_name = self.data['instance']['name']
-    self.instance_type = self.data['instance']['type']
-
-  def __constructSG(self):
-    self.prefix      = self.data['security_group']['prefix']
-    self.description = self.data['security_group']['description']
-    self.ports       = self.data['security_group']['ports']
-
-  def __createDir(self, template, system):
-    if ".yml" in template:
-      extension  = template.find('.yml')
-      parent_dir = template[0:extension] + '/' + system 
-      try:
-        if not os.path.exists(parent_dir):
-          os.makedirs(parent_dir)
-          
-        return parent_dir
-     
-      except:
-          print("Directory " + parent_dir + " could not be created!")
+  def __constructVars(self, category, *args):
+    try:
+      options = []
+      for option in args:  
+        if self.data[category][option] is not None:
+          options.append(self.data[category][option])
+        else:
+          print option + " is blank!  Blank options are not supported."
           sys.exit(1)
+    except KeyError, Argument:
+      print "No value specified for " + category + ":" + Argument[0]
+      sys.exit(1)
+    
+    return options
+      
+  def __createDir(self, template, system):
+    parent_dir = os.path.dirname(template) + '/' + system 
+    try:
+      if not os.path.exists(parent_dir):
+        os.makedirs(parent_dir)
+        return parent_dir
+    except:
+      print("Directory " + parent_dir + " could not be created!")
+      sys.exit(1)
 
   def __readSource(self, source_file):
     with open(source_file) as f:
@@ -74,33 +63,36 @@ class terraform_generator():
     
     return access_key, secret_key
 
-  def writeVars(self, source_file):
+  def __getDestFile(self, source_file):
     dest_file = self.parent_dir + "/" + os.path.basename(source_file)
     content   = Template(self.__readSource(source_file))
-  
+    return dest_file, content
+
+  def writeVars(self, source_file):
+    dest_file, content                      = self.__getDestFile(source_file) 
+    region, vpc, subnet, ami, keypair, user = self.__constructVars('variables', 'region', 'vpc', 'subnet', 'ami', 'keypair', 'user')
     with open(dest_file, "w+") as f:
-      f.write(content.render(region=self.region, vpc=self.vpc, subnet=self.subnet, ami=self.ami, keypair=self.keypair, user=self.user))
+      f.write(content.render(region=region, vpc=vpc, subnet=random.choice(subnet.values()), ami=ami, keypair=keypair, user=user)) 
+         
 
   def writeInstance(self, source_file):
-    dest_file = self.parent_dir + "/" + os.path.basename(source_file)
-    content   = Template(self.__readSource(source_file))
-
+    dest_file, content           = self.__getDestFile(source_file)
+    instance_name, instance_type = self.__constructVars('instance', 'name', 'type')
     with open(dest_file, "w+") as f:
-      f.write(content.render(instance_name=self.instance_name, instance_type=self.instance_type))
+      f.write(content.render(instance_name=instance_name, instance_type=instance_type))
 
   def writeSG(self, source_file):
-    dest_file = self.parent_dir + "/" + os.path.basename(source_file)
-    content   = Template(self.__readSource(source_file))
-
+    dest_file, content         = self.__getDestFile(source_file)
+    instance_name              = ''.join(self.__constructVars('instance', 'name'))
+    prefix, description, ports = self.__constructVars('security_group', 'prefix', 'description', 'ports')
     with open(dest_file, "w+") as f:
-      f.write(content.render(name=self.instance_name, prefix=self.prefix, description=self.description, ports=self.ports))
+      f.write(content.render(name=instance_name, prefix=prefix, description=description, ports=ports))
 
   def writeCredentials(self, source_file):
-    dest_file = self.parent_dir + '/' + os.path.basename(source_file)
-    content   = Template(self.__readSource(source_file))
-
+    dest_file, content     = self.__getDestFile(source_file)
+    access_key, secret_key = self.__getAWSCredentials()
     with open(dest_file, "w+") as f:
-      f.write(content.render(access_key=self.access_key, secret_key=self.secret_key))
+      f.write(content.render(access_key=access_key, secret_key=secret_key))
 
   def writeMetaData(self):
     for source in [ "templates/.gitignore", "templates/aws_config.tf" ]:
