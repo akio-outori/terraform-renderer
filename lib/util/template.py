7166f098db3cb7f3bfcb35832fc399ac8ff26270
Jeff Hallyburton <jeff.hallyburton@thomsonreuters.com>
2017-03-10 17:59:17 -0500

    Add support for elastic ip assignment on instance start, bastion hosts, and a user supplied configuration script which would be copied to the instance and executed.

--- lib/util/template.py
+++ lib/util/template.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import os
-from jinja2 import Template
+from jinja2 import Template, Environment
 from files import read as read_file
 
 def read(source_file, parent_dir):
     dest_file = parent_dir + "/" + os.path.basename(source_file)
     content   = Template(read_file(source_file))
+    
+    content.environment = Environment(trim_blocks=True)
     return dest_file, content
--- templates/terraform/instance.tf
+++ templates/terraform/instance.tf
@@ -3,17 +3,38 @@ resource "aws_instance" "{{ instance['name'] }}" {
   instance_type   = "{{ instance['type'] }}"
   subnet_id       = "${var.aws_subnet}"
   security_groups = ["${aws_security_group.{{ instance['name'] }}.id}"]
+  {% if instance['associate_public_ip'] == "true" -%}
+  associate_public_ip_address = true
+  {% endif -%}
   key_name        = "${var.aws_keypair}"
 
+  provisioner "file" {
+    connection {
+      user         = "${var.aws_user}"
+      agent        = true
+      {% if instance['bastion_host'] -%}
+      bastion_host = "{{ instance['bastion_host'] }}"
+      bastion_user = "${var.aws_user}"
+      {% endif %}
+    }
+   
+    source      = "files/{{ instance['config_script']}}"
+    destination = "~/{{ instance['config_script'] }}"
+  }
+  
   provisioner "remote-exec" {
     connection {
-      user        = "${var.aws_user}"
-      agent       = true
-      private_key = "${file("~/.ssh/${var.aws_keypair}")}"
+      user         = "${var.aws_user}"
+      agent        = true
+      {% if instance['bastion_host'] -%}
+      bastion_host = "{{ instance['bastion_host'] }}"
+      bastion_user = "${var.aws_user}"
+      {% endif %}
     }
 
     inline = [
-      "sudo echo {{ instance['name'] }}"
+      "chmod +x ~/{{ instance['config_script'] }}",
+      "sudo ~/{{ instance['config_script'] }}"
     ]
   }
 
@@ -21,8 +42,14 @@ resource "aws_instance" "{{ instance['name'] }}" {
 
 resource "aws_eip" "{{ instance['name'] }}_ip" {
   instance = "${aws_instance.{{ instance['name'] }}.id}"
+  vpc      = true
 }
 
-output "ip" {
+output "public_ip" {
   value = "${aws_eip.{{ instance['name'] }}_ip.public_ip}"
 }
+
+output "private_ip" {
+  value = "${aws_instance.{{ instance['name'] }}.private_ip}"
+}
+
